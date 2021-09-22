550dbfea066961cc36532799bb83da7e7a5adf7c
11telisz <telisof99@gmail.com>
2021-09-22 15:09:02 -0500

    Final touches and playing with back-ticks

--- default.js
+++ default.js
@@ -1,22 +1,34 @@
 function getData1() {
+
+    //We re-use this base function a lot.
+    // Eventually I'll figure our how to get them to run when I need them in this parent function. --Dan had a good way
+
+    //Basically this will grab our JSON Array from our server
     let xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://3.21.225.172:8080/api/realestate/all");
     xhttp.send();
     xhttp.onload = function() {
+
+        // This is creating a JS Object to contain our JSON information parsed out
         const houseData = JSON.parse(xhttp.responseText);
         // console.log(houseData);
+
+        //Now we can manipulate it here, in this case we are running houseCard() on our JS objects
         houseCard(houseData)
     };
     // console.log( xhttp)
 };
 
 function getData2() {
+
+    //This runs to grab all the information on our listing page
     let xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://3.21.225.172:8080/api/realestate/all");
     xhttp.send();
     xhttp.onload = function() {
         const houseData = JSON.parse(xhttp.responseText);
-        // console.log(houseData);
+        // console.log(houseData) will return an Array of objects
+        //but now we are running houseDetail() function
         houseDetail(houseData);
     };
     // console.log( xhttp)
@@ -47,6 +59,8 @@ function getData4() {
     // console.log( xhttp)
 };
 
+
+//Here is our function to create the cards shown on the listings page
 function houseCard(data){
     //Define all re-useable variables that will be pasted as HTML code later
     let divStart1 = "<div class=' color2  my-4 p-3'><div class='bg-info'><a href='home_detail.html?";
@@ -54,57 +68,59 @@ function houseCard(data){
     let divStart2 =  " ' ></img></a> </div><div class='color3 '>" ;
     let imgStart = "<img width='100%' src='";
     let url = "http://3.21.225.172:8080/api/";
+
+    //First clear whatever is in our container
     $("#Holder1").html("");
+
+    //Here we reset our input fields for lower value and upper value
     $("#pLower").val("");
     $("#pUpper").val("");
-    for (let i = 0; i < 90; i++) {
-        //define the card to be repeated using html as String and integrating our changing variables
+
+
+    //This is our for loop set to run for the length of our data
+    for (let house of data) {
+    // (let i = 0; i < data.length; i++) {
+        //Define the card to be repeated using html as String and integrating our changing variables
+        //Using the back-tick we can integrate some 'string interpolation' (fun word)
         var card =
-            "<div id='poppaDiv"+ [i] +"'  class='container  col-md-4'>"+ divStart1 + [i] + html1 + url + data[i].imageurl + divStart2 +
-            "<p type='value' id='price"+ [i] +"'>" +data[i].price +"</p><p>" + data[i].beds + " Bed    " +
-            data[i].baths + " Bath  " + data[i].sqft+ " Sq.ft</p>" +
-            "<p>" + data[i].street + "<br>"+ data[i].city + ", " + data[i].state + " " +
-            data[i].zip + "</p></div></div></div>"
+            `<div id='poppaDiv${house.id}' class='container  col-md-4'> ${divStart1}${data.indexOf(house)}${html1}${url}${house.imageurl}${divStart2}
+            <p type='value' id='price${data.indexOf(house)}'>Price: $${house.price}</p><p>${house.beds} Bed
+            ${house.baths} Bath<br>${house.sqft} Sq.ft</p>
+            <p>${house.street}<br>${house.city}, ${house.state}
+            ${house.zip}</p></div></div></div>`
+
 
 
         // console.log(url);
         // console.log(card);
         $("#Holder1").append(card);
     };
-}
-
-function houseCard2(data){
-
-    let i = 0 ;
-    let rep = data[i].fname + " " + data[i].lname;
-    let address = data[i].street + "<br>" + data[i].city + ", " + data[i].state + " " + data[i].zip ;
-    let img = "<img width='100%' src='http://3.21.225.172:8080/api/" + data[i].imageurl + "'></img>" ;
-    console.log(img);
-
-    $("#img1").html(img);
-    $("#price1").append(data[i].price);
-    $("#bed1").append(data[i].beds);
-    $("#bath1").append(data[i].baths);
-   $("#rep").append(rep);
-   $("#repPhone").append(data[i].phone);
-   $("#company").append(data[i].listing);
-    $("#address1").append(address);
-   $("#yrBlt").append(data[i].yrblt);
-
-
-
-}
+    // console.log(house);
+};
 
 function houseDetail(data){
     //define the detail page
 
- let lv1 =  window.location.search
-    console.log(lv1)
-     let i = lv1.slice(1);
+    let lv1 =  window.location.search;
+    console.log(lv1);
+    let i = lv1.slice(1);
+    console.log(i);
     let rep = data[i].fname + " " + data[i].lname;
     let address = data[i].street + "<br>" + data[i].city + ", " + data[i].state + " " + data[i].zip ;
     let img = "<img width='100%' src='http://3.21.225.172:8080/api/" + data[i].imageurl + "'></img>" ;
-   console.log(i);
+
+
+    //This is some testing code I was playing with
+    // for (let house of data) {
+
+        // for (key of houseKeys) {
+        //     for (value of houseValues) {
+        //         $("#id").append(`${house.id}`);
+            // }
+        // }
+    // };
+    //End of my testing code
+
 
 $("#img").html(img);
 $("#price").append(data[i].price);
@@ -120,17 +136,13 @@ $("#id").append(data[i].id);
 console.log(lv1);
 }
 function highestPrice(data){
-    // let img = "'><img width='100%' src='http://3.21.225.172:8080/api/" + data[0].imageurl + "'></img></a>" ;
     let link1 = "<a href='home_detail.html?";
     data.sort((a, b) => {
-        // let keyA = a.price;
-        // let keyB = b.price;
         if (a.price < b.price) {
             return 1 ;
         }else if ( a.price > b.price ){
             return -1;
         }else return 0;
-        // console.log(data);
     });
     let img = "'><img width='100%' src='http://3.21.225.172:8080/api/" + data[0].imageurl + "'></img></a>" ;
     let id1 = data[0].id
@@ -145,10 +157,6 @@ function highestPrice(data){
     $("#bath").append(data[0].baths);
     $("#tid").append(data[0].id);
     $("#feet").append(data[0].sqft);
-    // $("#company").append(data[i].listing);
-    // $("#address").append(address);
-    // $("#yrBlt").append(data[i].yrblt);
-
 };
 
 function filterResults(data) {
@@ -160,7 +168,7 @@ function filterResults(data) {
     let divStart2 =  " ' ></img></a> </div><div class='color3'>" ;
     let imgStart = "<img width='100%' src='";
     let url = "http://3.21.225.172:8080/api/";
-    console.log(pLower);
+    // console.log(pLower);
 
     //here we are checking if our Lower and Upper price fields have at least 5 length ie "500,000" or "999,999" or "000,000"  or 6 digit length
     if ((pLower.length <= 5) || (pUpper.length <= 5)){
@@ -170,16 +178,16 @@ window.alert("A minimum and maximum price must be at least 6 digits long.")
         //First we need to clear the current #Holder1 as it currently has all houses in it
         $("#Holder1").html("");
 
-
+        console.log(y)
    //for loop from our previous houseCard()
-    for (let i = 0; i < 90; i++) {
+    for (let i = 0; i < data.length; i++) {
         //define the card to be repeated
         var card =
-            "<div id='poppaDiv"+ [i] +"' class='container col-md-4'>"+divStart1 + [i] + html1 + url + data[i].imageurl + divStart2 +
-            "<p type='value' id='price"+ [i] +"'>" +data[i].price +"</p><p>" + data[i].beds + " Bed    " +
-            data[i].baths + " Bath  " + data[i].sqft+ " Sq.ft</p>" +
-            "<p>" + data[i].street + "<br>"+ data[i].city + ", " + data[i].state + " " +
-            data[i].zip + "</p></div></div></div>"
+            `<div id='poppaDiv${i}' class='container  col-md-4'> ${divStart1}${i}${html1}${url}${data[i].imageurl}${divStart2}
+            <p type='value' id='price${[i]}'>${data[i].price}</p><p>${data[i].beds}Bed    
+            ${data[i].baths}Bath  ${data[i].sqft} Sq.ft</p>
+            <p>${data[i].street}<br>${data[i].city}, ${data[i].state} 
+            ${data[i].zip}</p></div></div></div>`
 
         //Now that our card is defined and our data is loaded we can start applying our filters to it.
         //We only want the ones that fail our test. it is a strange use of double negatives
